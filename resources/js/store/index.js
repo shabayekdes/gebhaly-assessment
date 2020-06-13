584b3bb24bebff5ef5f36e1323b9688e3c3e6aa8
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-06-13 18:08:23 +0200

    Add store index state managment

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,15 +1,48 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Service from "./modules/service";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
+        meta_data: {
+            last_page: null,
+            current_page: 1,
+            prev_page_url: null
+        },
+        errors: {}
     },
     getters: {
+        getMetaData: state => state.meta_data,
+        getErrorMsg: state => field => {
+            if (state.errors.hasOwnProperty(field)) {
+                return state.errors[field][0];
+            }
+        },
+        hasError: state => field => {
+            return state.errors.hasOwnProperty(field);
+        }
     },
     actions: {
+        setError({ commit }, oldError) {
+            commit("SET_ERRORS", oldError);
+        }
     },
     mutations: {
+        SET_META_DATA: (state, data) => {
+            state.meta_data.from = data.meta.from;
+            state.meta_data.to = data.meta.to;
+            state.meta_data.total = data.meta.total;
+            state.meta_data.last_page = data.meta.last_page;
+            state.meta_data.current_page = data.meta.current_page;
+            state.meta_data.prev_page_url = data.links.prev;
+        },
+        SET_ERRORS: (state, data) => {
+            state.errors = data;
+        }
+    },
+    modules: {
+        Service 
     }
 });
