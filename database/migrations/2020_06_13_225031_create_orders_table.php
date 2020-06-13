9db2551b4d73b5baa9f21170bcdbafe93a4ede1e
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-06-14 01:00:45 +0200

    Add order migration table

---
+++ database/migrations/2020_06_13_225031_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_email');
+            $table->string('customer_name');
+            $table->boolean('is_guest')->default(false);
+            $table->foreignId('user_id')->nullable()->references('id')->on('users')->onDelete('set null');
+            $table->string('session_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
